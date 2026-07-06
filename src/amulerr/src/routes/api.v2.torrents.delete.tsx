f47b36114e9ce77a19ff668c54f8ccbf00c914ce
isc30 <ivansanzcarasa@gmail.com>
2026-07-06 16:56:21 +0200

    Honor "deleteFiles" flag in torrent/delete api , partial fix from #51

--- src/amulerr/src/routes/api.v2.torrents.delete.tsx
+++ src/amulerr/src/routes/api.v2.torrents.delete.tsx
@@ -1,7 +1,8 @@
-
 import { useAmule } from '#/amule'
 import { skipFalsy } from '#/lib/array'
 import { createFileRoute } from '@tanstack/react-router'
+import fs from 'node:fs/promises'
+import path from 'node:path'
 
 export const Route = createFileRoute('/api/v2/torrents/delete')({
   server: {
@@ -9,28 +10,48 @@ export const Route = createFileRoute('/api/v2/torrents/delete')({
       POST: async ({ request }) => {
         const formData = await request.formData()
         const hashes = formData
-          .get("hashes")
+          .get('hashes')
           ?.toString()
           ?.toUpperCase()
-          ?.split("|")
+          ?.split('|')
           .filter(skipFalsy)
+        const deleteFiles = formData.get('deleteFiles')?.toString() === 'true'
 
         if (hashes?.length) {
           await useAmule(async (amule) => {
             const shared = await amule.getSharedFiles()
-            const ecids = shared
-              .filter(f => f.fileHash && hashes.includes(f.fileHash.toUpperCase()))
-              .map(f => f.ecid).filter(skipFalsy)
+            const matches = shared.filter(
+              (f) => f.fileHash && hashes.includes(f.fileHash.toUpperCase()),
+            )
 
+            const ecids = matches.map((f) => f.ecid).filter(skipFalsy)
             await amule.clearCompleted(ecids)
+
             for (const hash of hashes) {
               await amule.cancelDownload(hash)
+            }
+
+            if (deleteFiles) {
+              const files = matches
+                .filter((f) => f.path && f.fileName)
+                .map((f) => path.join(f.path!, f.fileName!))
+
+              await Promise.allSettled(
+                files.map((f) =>
+                  fs.rm(f, { force: true }).catch((err) => {
+                    console.error(`Failed to delete file ${f}:`, err)
+                    return Promise.reject(err)
+                  }),
+                ),
+              )
+
+              await amule.refreshSharedFiles()
             }
           })
         }
 
         return Response.json({})
-      }
-    }
+      },
+    },
   },
 })
